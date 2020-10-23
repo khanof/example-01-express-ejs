32d50041ff849a5ab7c627f7e03347d73257f9ec
amir khanof <amir.khanof@royalvision.ir>
2020-10-22 22:17:31 -0400

    finished the tasks on readme.txt

---
+++ models/user.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+var passportLocalMongoose = require('passport-local-mongoose')
+
+var UserSchema = new mongoose.Schema({
+	username: String,
+	email: String,
+	password: String
+})
+
+UserSchema.plugin(passportLocalMongoose, {
+	usernameField: 'username',
+	usernameQueryFields:['email'] //additional field to query the username
+})
+
+module.exports = mongoose.model('User', UserSchema)
+
+// Somewhere in this file you can change or add to allow for the user to login with their email address
